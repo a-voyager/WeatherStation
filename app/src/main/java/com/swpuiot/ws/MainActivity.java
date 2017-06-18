377a93970c0973ad391c443a6fa978c918a17900
yangnyear <yangrongyi_em@163.com>
2017-06-18 18:02:43 +0800

    bindview

--- app/src/main/java/com/swpuiot/ws/MainActivity.java
+++ app/src/main/java/com/swpuiot/ws/MainActivity.java
@@ -2,6 +2,8 @@ package com.swpuiot.ws;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.swpuiot.ws.base.BaseActivity;
@@ -9,8 +11,28 @@ import com.swpuiot.ws.base.BaseActivity;
 import butterknife.BindView;
 
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity  {
+    @BindView(R.id.tt_window_temp)
+    TextView showTemp;
 
+    @BindView(R.id.tt_window_weather_stat)
+    TextView weartherStat;
+    @BindView(R.id.tt_window_address)
+    TextView userAdd;
+
+    @BindView(R.id.tt_window_air_quality)
+    TextView airQuality;
+    @BindView(R.id.tt_window_air_temp)
+    TextView airTemp;
+    @BindView(R.id.tt_window_air_hump)
+    TextView airHump;
+
+    @BindView(R.id.ll_window_air_quality)
+    LinearLayout qualityLayout;
+    @BindView(R.id.ll_window_air_temp)
+    LinearLayout tempLayout;
+    @BindView(R.id.ll_window_air_hump)
+    LinearLayout humpLayout;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
