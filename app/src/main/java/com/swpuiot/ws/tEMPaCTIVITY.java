858686f7097f2d4219378f75b85585e950daf5a9
yangnyear <yangrongyi_em@163.com>
2017-06-19 07:58:43 +0800

    fix

---
+++ app/src/main/java/com/swpuiot/ws/tEMPaCTIVITY.java
@@ -0,0 +1,18 @@
+package com.swpuiot.ws;
+
+import com.swpuiot.ws.base.BaseActivity;
+
+/**
+ * Created by 羊荣毅_L on 2017/6/19.
+ */
+public class TempActivity extends BaseActivity {
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected int getLayoutResID() {
+        return R.layout.activity_temp;
+    }
+}
