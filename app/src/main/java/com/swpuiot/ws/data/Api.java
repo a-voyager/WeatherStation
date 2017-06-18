4e37c5f24d5a1890ea734e69e48052c5c4c859c8
wuhaojie <w19961009@126.com>
2017-06-18 12:20:03 +0800

    初始化 API

---
+++ app/src/main/java/com/swpuiot/ws/data/Api.java
@@ -0,0 +1,15 @@
+package com.swpuiot.ws.data;
+
+/**
+ * Author: wuhaojie
+ * E-mail: w19961009@126.com
+ * Date: 2017/06/18 12:15
+ * Version: 1.0
+ */
+
+public interface Api {
+
+    String BASE_URL = "";
+
+
+}
---
+++ app/src/main/java/com/swpuiot/ws/data/HttpHelper.java
@@ -0,0 +1,16 @@
+package com.swpuiot.ws.data;
+
+/**
+ * Author: wuhaojie
+ * E-mail: w19961009@126.com
+ * Date: 2017/06/18 12:18
+ * Version: 1.0
+ */
+
+public class HttpHelper {
+
+    private HttpHelper() {
+    }
+
+
+}
