cdfc1f05caff5bd88bb3ca3bad6648aa4f85356c
wuhaojie <w19961009@126.com>
2017-06-19 08:12:38 +0800

    生活建议 加入新 Activity

--- app/src/main/java/com/swpuiot/ws/TemperatureActivity.java
+++ app/src/main/java/com/swpuiot/ws/TemperatureActivity.java
@@ -3,15 +3,22 @@ package com.swpuiot.ws;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.swpuiot.ws.base.BaseActivity;
+import com.swpuiot.ws.data.HttpHelper;
+import com.swpuiot.ws.entities.response.SuggestResponse;
 
 public class TemperatureActivity extends BaseActivity {
+
+    private static final String TAG = "TemperatureActivity";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        HttpHelper.get().suggestion("成都", "zh", this::handleSuggestResponse);
     }
+
 
     @Override
     protected void initViews() {
@@ -22,4 +29,10 @@ public class TemperatureActivity extends BaseActivity {
     protected int getLayoutResID() {
         return R.layout.activity_temperature;
     }
+
+
+    private void handleSuggestResponse(SuggestResponse suggestResponse) {
+        Log.d(TAG, "call: " + suggestResponse);
+    }
+
 }
